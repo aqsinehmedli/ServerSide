e46f393398b40284bfba97d362786de02687d329
aqsinehmedli <aqsinhmdli03@gmail.com>
2024-10-09 23:14:24 +0400

    Add project files.

---
+++ ClientSide/Program.cs
@@ -0,0 +1,60 @@
+﻿
+
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+Console.WriteLine("Client");
+
+var ipAddress = IPAddress.Parse("192.168.1.99");
+var port = 27001;
+var endPoint = new IPEndPoint(ipAddress, port);
+
+while (true)
+{
+    using (var client = new TcpClient())
+    {
+        try
+        {
+            client.Connect(endPoint);
+            if (client.Connected)
+            {
+                Console.WriteLine("Client bağlı..");
+
+                using (Bitmap memoryImage = new Bitmap(1920, 1080))
+                {
+                    using (Graphics memoryGraphics = Graphics.FromImage(memoryImage))
+                    {
+                        memoryGraphics.CopyFromScreen(0, 0, 0, 0, memoryImage.Size);
+                    }
+
+                    var networkStream = client.GetStream();
+                    string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+                    string path = $"ScreenShot_{timestamp}.png";
+                    using (var memoryStream = new MemoryStream())
+                    {
+
+
+                        memoryImage.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+                        var buffer = memoryStream.ToArray();
+                        networkStream.Write(buffer, 0, buffer.Length);
+
+                        var fileNameBytes = System.Text.Encoding.UTF8.GetBytes(path);
+                        networkStream.Write(fileNameBytes, 0, fileNameBytes.Length);
+                    }
+
+
+                    Console.WriteLine($"Ekran görüntüsü kaydedildi: {path}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Hata: " + ex.Message);
+        }
+
+        Thread.Sleep(10000);
+    }
+}
+
---
+++ ServerSide/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+
+Console.WriteLine("Server:");
+
+var ip = IPAddress.Parse("192.168.1.99");
+var port = 27001;
+var endPoint = new IPEndPoint(ip, port);
+
+var listener = new TcpListener(endPoint);
+
+try
+{
+    listener.Start();
+    Console.WriteLine("Listener starting...");
+    while (true)
+    {
+        var client = listener.AcceptTcpClient();
+        _ = Task.Run(() =>
+        {
+            var stream = client.GetStream();
+            string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+            string path = $"ScreenShot_{timestamp}.png";
+
+            using (var readFs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                int len = 0;
+                var bytes = new byte[1024];
+                while ((len = stream.Read(bytes, 0, bytes.Length)) > 0)
+                {
+                    readFs.Write(bytes, 0, len);
+                }
+            }
+            if (File.Exists(path))
+            {
+                Console.WriteLine("Dosya başarıyla kaydedildi: " + path);
+            }
+            else
+            {
+                Console.WriteLine("Dosya kaydedilemedi.");
+            }
+
+            Console.WriteLine("File received");
+            client.Close();
+
+        });
+
+    }
+
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
